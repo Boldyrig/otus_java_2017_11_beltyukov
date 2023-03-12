f11bebfbdf2e29f9f94ef4de14495f98410e69f0
Andrew Beltyukov <fuskerr63@gmail.com>
2023-03-12 10:03:17 +0400

    Зависимости + jar + .gitignore

--- src/main/java/com/fuskerr/Main.java
+++ src/main/java/com/fuskerr/Main.java
@@ -1,5 +1,15 @@
-package com.fuskerr;public class Main {
+package com.fuskerr;
+
+import com.google.common.collect.AbstractIterator;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        AbstractIterator<Object> iterator = new AbstractIterator<>() {
+            @Override
+            protected Object computeNext() {
+                return null;
+            }
+        };
     }
 }
